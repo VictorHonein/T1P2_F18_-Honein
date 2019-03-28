f5e8cde3ec26886b6aa08e6ac8fcd36b53c9a980
Victor Honein <vshonein@gmail.com>
2019-03-28 11:01:02 +0200

    fourth version

---
+++ delete.sh
@@ -0,0 +1,9 @@
+rm Hello.aux
+rm Hello.log             
+rm Hello.out             
+rm Hello.synctex.gz      
+rm Hello.tmp             
+rm Hello.gz              
+rm Hello.nav             
+rm Hello.synctex         
+rm Hello.toc  
